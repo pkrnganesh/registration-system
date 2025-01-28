69978f50fde924022281c30bfded96a1e2ca314c
pkrnganesh <pothamsettikodanda@gmail.com>
2025-01-28 10:33:41 +0530

    index.php

---
+++ admin/index.php
@@ -0,0 +1,13 @@
+<?php include '../includes/db.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Dashboard</title>
+</head>
+<body>
+    <h2>Admin Dashboard</h2>
+    <a href="view-registrations.php">View Registrations</a>
+</body>
+</html>
